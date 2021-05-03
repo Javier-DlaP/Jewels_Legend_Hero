ecac4c874a3f8bd6ce9fadbb7248aeaf50369bdd
Javi <47185380+Javier-DlaP@users.noreply.github.com>
2021-05-03 16:08:18 +0200

    Funciones básicas de listas actualizadas
    Funciones básicas de listas actualizadas para permitir matrices que no sean cuadradas

--- src/Funciones_lista.scala
+++ src/Funciones_lista.scala
@@ -1,23 +1,26 @@
-class Funciones_lista {
-  
-    def imprimir(length: Int, matrix: List[Int]) {
-      def imprimir_aux(width: Int, matrix: List[Int]): List[Int] = {
-        if (width == 0) {
-          matrix
-        } else {
-          print(matrix.head + " ")
-          imprimir_aux(width - 1, matrix.tail)
-        }
-      }
+object Funciones_lista {
 
-      if (matrix != Nil) {
-        val matrix_ = imprimir_aux(length, matrix)
-        println()
-        imprimir(length, matrix_)
+  // Impresión de una lista en forma de matriz a partir de esta y sus dimensiones
+  def imprimir_lista(width: Int, length: Int, matrix: List[Int]): Unit = {
+    // Función auxiliar que imprime para una de las filas
+    def imprimir_lista_aux(width: Int, matrix: List[Int]): List[Int] = {
+      if (width == 0) {
+        matrix
+      } else {
+        print(matrix.head + " ")
+        imprimir_lista_aux(width - 1, matrix.tail)
       }
     }
+    if (length != 0) {
+      val matrix_ = imprimir_lista_aux(width, matrix)
+      println()
+      imprimir_lista(width, length-1, matrix_)
+    }
+  }
 
-  def get(x: Int, y: Int, length: Int, matrix: List[Int]): Int = {
+  // Obtiene el valor de una posición concreta de una lista en forma de matriz a partir de la posición, la matriz y sus dimensiones
+  def get(x: Int, y: Int, width: Int, length: Int, matrix: List[Int]): Int = {
+    // Función auxiliar que busca el valor requerido de forma iterativa
     def get_aux(n: Int, matrix: List[Int]): Int = {
       if (n == 0) {
         matrix.head
@@ -25,10 +28,13 @@ class Funciones_lista {
         get_aux(n-1, matrix.tail)
       }
     }
-    get_aux(x*length+y, matrix)
+    assert(x<length && y<width)
+    get_aux(x*width+y, matrix)
   }
 
-  def row(x: Int, length: Int, matrix: List[Int]): List[Int] = {
+  // Obtiene la fila especificada a partir de la requerida, la matriz y sus dimensiones
+  def row(x: Int, width: Int, length: Int, matrix: List[Int]): List[Int] = {
+    // Función auxiliar que que itera por cada uno de los elementos de la lista guardando los necesarios
     def row_aux(x: Int, a: Int, b: Int, matrix: List[Int]): List[Int] = {
       if (x >= a && x <= b) {
         matrix.head :: row_aux(x+1, a, b, matrix.tail)
@@ -40,21 +46,25 @@ class Funciones_lista {
         }
       }
     }
-    row_aux(0, x*length, (x+1)*length-1, matrix)
+    assert(x<length)
+    row_aux(0, x*width, (x+1)*width-1, matrix)
   }
 
-  def column(y: Int, length: Int, matrix: List[Int]): List[Int] = {
-    def column_aux(x: Int, y: Int, matrix: List[Int]): List[Int] = {
-      if (x%length == y) {
-        matrix.head :: column_aux(x+1, y, matrix.tail)
+  // Obtiene la columna especificada a partir de la requerida, la matriz y sus dimensiones
+  def column(y: Int, width: Int, length: Int, matrix: List[Int]): List[Int] = {
+    // Función auxiliar que que itera por cada uno de los elementos de la lista guardando los necesarios
+    def column_aux(x: Int, y: Int, width: Int, length: Int, matrix: List[Int]): List[Int] = {
+      if (x%width == y) {
+        matrix.head :: column_aux(x+1, y, width, length, matrix.tail)
       } else {
-        if ((length*(length-1)+y)+1 == x) {
+        if ((width*(length-1)+y)+1 == x) {
           Nil
         } else {
-          column_aux(x+1, y, matrix.tail)
+          column_aux(x+1, y, width, length, matrix.tail)
         }
       }
     }
-    column_aux(0, y, matrix)
+    assert(y<width)
+    column_aux(0, y, width, length, matrix)
   }
 }
--- src/Funciones_tablero.scala
+++ src/Funciones_tablero.scala
@@ -1,3 +1,3 @@
-class Funciones_tablero {
+object Funciones_tablero {
 
 }
